6ba9f93a18f44738340aefd949ee9804fa17eb99
Daniel Liberato de Jesus <Daniel.Liberato-ext@ab-inbev.com>
2022-09-23 22:18:34 -0300

    Added healthcheck endpoint

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI, APIRouter
 from dotenv import load_dotenv
+from fastapi_health import health
 
 from src.web.api import api_router
+
+
+def healthy_condition():
+    return {"service": "online"}
+
 
 load_dotenv()
 
 root_router = APIRouter()
 app = FastAPI(title="UNIP 2022 - TCC")
+app.add_api_route("/health", health([healthy_condition]))
 
 app.include_router(api_router)
 app.include_router(root_router)
--- src/web/api.py
+++ src/web/api.py
@@ -6,3 +6,4 @@ from src.web.endpoints import alert, movie
 api_router = APIRouter()
 api_router.include_router(alert.router, prefix="/alert", tags=["alert"])
 api_router.include_router(movie.router, prefix="/movie", tags=["movie"])
+
